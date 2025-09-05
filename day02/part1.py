33c633762ad24ef233d6a9cbe6b03c5a1dab1c6c
Ian Contijoch <ian.contijoch@gmail.com>
2025-09-05 17:57:06 +0200

    day02-part1

---
+++ day02/part1.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    total = 0
+    for line in lines:
+        lst = list(map(int, line.split()))
+        total += max(lst) - min(lst)
+
+    return total
+
+
+INPUT_S = '''\
+5 1 9 5
+7 5 3
+2 4 6 8
+'''
+EXPECTED = 18
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
