444be95864d86df0ae05c5d3438ea39cc60d436a
Ian Contijoch <ian.contijoch@gmail.com>
2026-01-01 23:51:29 +0100

    day14-part1

---
+++ day14/part1.py
@@ -0,0 +1,130 @@
+from __future__ import annotations
+
+import argparse
+import itertools
+import os.path
+from functools import reduce
+from operator import xor
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+N_ELEMS = 256
+
+
+def get_window(
+    lst: list[int],
+    i: int, window_size: int,
+) -> tuple[list[int], int, int]:
+    lst_len = len(lst)
+    window_end = i + window_size
+    if window_end > lst_len:
+        window_start, window_end = i, window_end - lst_len
+        right = lst[window_start:]
+        left = lst[:window_end]
+        return (right + left, window_start, window_end)
+    return (lst[i:window_end], i, window_end)
+
+
+def update_lst(lst: list[int], i: int, length: int) -> list[int]:
+    # get reversed window
+    window, window_start, window_end = get_window(lst, i, length)
+    reversed_window = window[::-1]
+
+    # window is zero, return list as-is
+    if length == 0:
+        # plus do stuff
+        return lst
+    # 2. if window end > window start, no wrap-around
+    if window_end > window_start:
+        return lst[:window_start] + reversed_window + lst[window_end:]
+    elif window_end <= window_start:
+        same = lst[window_end:i]
+        left = reversed_window[-window_end:]
+        right = reversed_window[:-window_end]
+        return left + same + right
+    else:
+        raise ValueError
+
+
+def get_lengths(s: str) -> list[int]:
+    return [ord(x) for x in s] + [17, 31, 73, 47, 23]
+
+
+def knot_hash(
+    s: str, n_elems: int,
+    lengths: list[int], rounds: int,
+) -> list[int]:
+    lst = list(range(n_elems))
+    len_lst = len(lst)
+    i = skip_size = 0
+
+    for _ in range(rounds):
+        for n in lengths:
+            lst = update_lst(lst, i, n)
+            i = (i + n + skip_size) % len_lst
+            skip_size += 1
+    return lst
+
+
+def compute_hash(s: str, n_elems: int) -> str:
+    s = s.strip()
+    lengths = get_lengths(s)
+    sparse_hash = knot_hash(s, n_elems, lengths, 64)
+
+    dense_hash = [
+        reduce(xor, sparse_hash)
+        for sparse_hash in itertools.batched(sparse_hash, n=16)
+    ]
+
+    hex_str = ''.join(str.zfill(hex(x)[2:], 2) for x in dense_hash)
+    return hex_str
+
+
+def hash_to_hex(s: str) -> list[int]:
+    ret = list(map(int, ''.join(bin(int(c, 16))[2:].zfill(4) for c in s)))
+    return ret
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    total = 0
+    for line in lines:
+        for i in range(128):
+            hash_s = compute_hash(f'{line.strip()}-{i}', N_ELEMS)
+            hex_s = hash_to_hex(hash_s)
+            total += sum(hex_s)
+    return total
+
+
+INPUT_S = '''\
+flqrgnkx
+'''
+EXPECTED = 8108
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
